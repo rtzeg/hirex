682c9586dd1b863a729852c17d908c7941b59f2f
rtzeg <82582174+rtzeg@users.noreply.github.com>
2026-02-11 04:16:36 +0500

    Add About Us and Workflow Process section above slider

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/layout/header.jsx";
+import Footer from "../components/layout/footer.jsx";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-slate-100 text-slate-900">
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
